de36839fd3da3054d41f153b0890ecba8826836b
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-03-12 13:06:33 -0300

    add react bealtiful dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Board from "./components/Board";
 import { Header } from "./components/Header";
 import GlobalStyle from "./styles/GlobalStyle";
+import { DragDropContext } from "react-beautiful-dnd";
 function App() {
   return (
-    <>
+    <DragDropContext>
       <GlobalStyle />
       <Header />
       <Board />
-    </>
+    </DragDropContext>
   );
 }
 
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListContainer } from "./style";
 import { MdAdd } from "react-icons/md";
 import Card from "../Card";
+import { Draggable, Droppable, DragDropContext } from "react-beautiful-dnd";
 
 export default function List({ data }) {
+  const [dataAPI, setDataAPI] = useState(data);
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+    const items = Array.from(dataAPI);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setDataAPI(items);
+  };
+
   return (
     <ListContainer done={data.done}>
       <header>
@@ -15,11 +27,34 @@ export default function List({ data }) {
           </button>
         )}
       </header>
-      <ul>
-        {data.cards.map((card) => (
-          <Card key={card.id} data={card} />
-        ))}
-      </ul>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId={"list"}>
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {dataAPI.cards.map((card, index) => {
+                return (
+                  <Draggable
+                    key={card.id}
+                    draggableId={card.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card key={card.id} data={card} />
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </ListContainer>
   );
 }
